a89f35e3c9b60b5d705951714ffe912daa65b580
Jam Ajna Soares <ajnasoares@hotmail.com>
2024-02-12 17:10:57 -0300

    Compare the Triplets (06/02/2024)

---
+++ Prepare/Algorithms/Warmup/Compare the Triplets/src/main.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+
+def compareTriplets(a, b):
+    A = 0
+    B = 0
+    result = []
+
+    i = 0
+    for point in a:
+        if a[i] > b[i]:
+            A += 1
+        elif a[i] < b[i]:
+            B += 1
+
+        i += 1
+
+    result.append(A)
+    result.append(B)
+
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
