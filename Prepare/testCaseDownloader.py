cdd14aea3a7e8eca50119ad7169eec95a845bd0f
Jam Ajna Soares <ajnasoares@hotmail.com>
2024-02-16 21:40:59 -0300

    Mini-max Sum (07/02/2024) test cases 10 a 14

--- Prepare/testCaseDownloader.py
+++ Prepare/testCaseDownloader.py
@@ -3,8 +3,8 @@ import requests
 TESTCASESFOLDER = "Prepare/Algorithms/Warmup/Mini-Max Sum/test cases"    # insert yours
 
 # INPUT AND OUTPUT URL'S OF GIVEN TESTCASE
-URL_ARRAY=["https://hr-testcases-us-east-1.s3.amazonaws.com/26276/input09.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240213%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240213T160455Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=b380a98b569ce0688800f7247778628876d5b15c1b8c22cee6ac5092ed9c842f",
-           "https://hr-testcases-us-east-1.s3.amazonaws.com/26276/output09.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240213%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240213T160457Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=29c050dce7daf3247f75a1ea8e7607684db3b5fc7724ed02328bafaaf31d157e"]
+URL_ARRAY=["https://hr-testcases-us-east-1.s3.amazonaws.com/26276/input14.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240217%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240217T003857Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=65444c85db29d25f4315cc04281a885b891a7db87a3cc7d708ea10dcf59dee76",
+           "https://hr-testcases-us-east-1.s3.amazonaws.com/26276/output14.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240217%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240217T003928Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=2d315bdecd80216c62153a61c3ad7344d6589ac74f149f3a7abe4f62b088d849"]
 
 for url in URL_ARRAY:
 
