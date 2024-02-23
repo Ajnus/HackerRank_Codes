5d0e5bec4666c21c5c5ee4454d4a859fa665bb9c
Ajnus <ajnasoares@hotmail.com>
2024-02-23 15:38:38 -0300

    Birthday Cake Candles (07/02/2024) até test case 04, implementa exibição do nome do Problema e destaque do texto na impressão na tela

--- Prepare/testCaseDownloader.py
+++ Prepare/testCaseDownloader.py
@@ -1,10 +1,13 @@
 import requests
 
-TESTCASESFOLDER = "Prepare/Algorithms/Warmup/Mini-Max Sum/test cases"    # insert yours
+PROBLEM = "Birthday Cake Candles"                                       # custom
+TESTCASESFOLDER = f"Prepare/Algorithms/Warmup/{PROBLEM}/test cases"     # insert yours
 
 # INPUT AND OUTPUT URL'S OF GIVEN TESTCASE
-URL_ARRAY=["https://hr-testcases-us-east-1.s3.amazonaws.com/26276/input14.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240217%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240217T003857Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=65444c85db29d25f4315cc04281a885b891a7db87a3cc7d708ea10dcf59dee76",
-           "https://hr-testcases-us-east-1.s3.amazonaws.com/26276/output14.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240217%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240217T003928Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=2d315bdecd80216c62153a61c3ad7344d6589ac74f149f3a7abe4f62b088d849"]
+URL_ARRAY=["https://hr-testcases-us-east-1.s3.amazonaws.com/23074/input04.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240223%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240223T163759Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=c569f3efa8df0a661d40c0d645a4a514ffd13975dbd97768a1034c6ffbf23b85",
+           "https://hr-testcases-us-east-1.s3.amazonaws.com/23074/output04.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240223%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240223T163820Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=d0d752855c2193a79ab697cc67bfeffb6218182c6d2971b815006b35aea96e06"]
+
+print(f"----------------------\n{PROBLEM}: ")
 
 for url in URL_ARRAY:
 
@@ -23,3 +26,5 @@ for url in URL_ARRAY:
     with open(FILEPATH, 'wb') as f:
         f.write(r.content)
     print(f"{mode}{case_number} done")
+
+print("----------------------")
