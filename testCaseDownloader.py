cbc60c821c616ce09d4f222fbcc72c4d062b0495
Ajnus <ajnasoares@hotmail.com>
2024-02-27 23:43:28 -0300

    Cats and a Mouse (C#) disponíveis all test cases

--- testCaseDownloader.py
+++ testCaseDownloader.py
@@ -1,11 +1,11 @@
 import requests
 
-PROBLEM = "Birthday Cake Candles"                                       # custom
-TESTCASESFOLDER = f"Prepare/Algorithms/Warmup/{PROBLEM}/test cases"     # insert yours
+PROBLEM = "Cats and a Mouse"                                       # custom
+TESTCASESFOLDER = f"Prepare/Algorithms/Implementation/{PROBLEM}/test cases"     # insert yours
 
 # INPUT AND OUTPUT URL'S OF GIVEN TESTCASE
-URL_ARRAY=["https://hr-testcases-us-east-1.s3.amazonaws.com/23074/input04.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240223%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240223T163759Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=c569f3efa8df0a661d40c0d645a4a514ffd13975dbd97768a1034c6ffbf23b85",
-           "https://hr-testcases-us-east-1.s3.amazonaws.com/23074/output04.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240223%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240223T163820Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=d0d752855c2193a79ab697cc67bfeffb6218182c6d2971b815006b35aea96e06"]
+URL_ARRAY=["https://hr-testcases-us-east-1.s3.amazonaws.com/29054/input01.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240228%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240228T023347Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=a90203f3fc63e0fd9ed5dfcb17628a817e2134a5f87c4f704964b66ecb48f430",
+           "https://hr-testcases-us-east-1.s3.amazonaws.com/29054/output01.txt?response-content-type=text%2Fplain&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX5DNFO3PV%2F20240228%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240228T023539Z&X-Amz-Expires=7200&X-Amz-SignedHeaders=host&X-Amz-Signature=37fc865ed67bdee054e2622aba49fcaf90a459ff3549f11fbc2fb8031dfaaebf"]
 
 print(f"----------------------\n{PROBLEM}: ")
 
